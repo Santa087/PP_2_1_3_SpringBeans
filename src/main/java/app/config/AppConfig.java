7080933283802aed23a1a88ea156a57d584f6930
Santa087 <141545868+Santa087@users.noreply.github.com>
2025-12-04 16:13:02 +0300

    Обновил проект

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,20 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.security.DigestException;
 
 @Configuration
 @ComponentScan(basePackages = "app")
+@Component
+@Qualifier("dog")
+
+
 public class AppConfig {
+
 }
